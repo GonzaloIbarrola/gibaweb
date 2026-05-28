51a88b03d23d34d05567099536fca13faf95b2a9
Gonzalo Ibarrola <gonzaloibarrola95@gmail.com>
2026-05-28 18:04:15 -0300

    .

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,12 @@
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/next";
-import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Head>
+    <>
       <link rel="icon" href="/favicon.png" />
       <Component {...pageProps} />
       <Analytics />
-    </Head>
+    </>
   );
 }
